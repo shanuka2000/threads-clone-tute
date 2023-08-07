d815d9fae8d1227e0191aa32cfe532d988162527
shanuka2000 <shanuka.inbox@gmail.com>
2023-08-08 01:51:15 +0530

    Updated layout for main view and implemented Topbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
+import BottomBar from "@/components/shared/BottomBar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import TopBar from "@/components/shared/TopBar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -18,7 +23,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <TopBar />
+          <main>
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <BottomBar />
+        </body>
       </html>
     </ClerkProvider>
   );
